2fd017f82c4195b6c455e3ca5ff64f540e3852ab
Federica Fiorenza <20091413@mail.wit.ie>
2021-08-04 00:46:19 +0100

    Adding API for Top Rated Movies, Popular Movies and Movies Credits/Cast

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -66,3 +66,37 @@ export const getMovie = async ( args ) => {
     return response.json();
   };
 
+  //adding API to retrieve Top Rated Movies
+  export const getTopRatedMovies = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    );
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  };
+
+  
+  //adding API to retrieve Popular Movies
+  export const getPopularMovies = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    );
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  };
+
+  //adding API to get Credits / Cast
+  export const getCast = async (id) => {
+    const fetchResponse = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    );
+    const res = await fetchResponse.json();
+    const json = res.cast;
+    return json;
+  };
+
+  
